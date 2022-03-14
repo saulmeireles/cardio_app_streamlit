a9924127b81b3fc805693e5442a00af1cbff5b68
Saul Meireles <saulm@live.com>
2022-03-13 22:17:23 -0300

    commmit app

--- cardio_app2.py
+++ cardio_app2.py
@@ -37,48 +37,52 @@ st.subheader("Características do Paciente")
 user_input = st.sidebar.text_input("Digite seu nome")
 
 st.write("Paciente: ", user_input)
+st.image('cardio_img.png')
 
 # Dados do paciente
 def user_input_features():
-    gender = st.sidebar.selectbox("Gênero", [1, 2], 1)
-    age = st.sidebar.slider("Idade", 15, 65, 20)
-    height = st.sidebar.slider("Altura", 0.5, 2.5, 1.70)
-    weight = st.sidebar.slider("Peso", 10, 200, 60)
-    ap_hi = st.sidebar.slider("Pressão sistólica", 0, 500, 100)
-    ap_lo = st.sidebar.slider("Pressão diastólica", 0, 500, 100)
-    cholesterol = st.sidebar.selectbox("Colesterol", [1,2 ,3], 1)
-    gluc = st.sidebar.selectbox("Glicose",[1,2, 3] ,1)
-    smoke = st.sidebar.selectbox("Fumante", [0, 1], 0)
-    alco = st.sidebar.selectbox("Bebida Alcoolica", [0 ,1], 0)
-    active = st.sidebar.selectbox("Praticante de atividade física", [0, 1], 0)
-
-    data = {"Gênero": gender,
-            "Idade": age,
-            "Altura": height,
-            "Peso": weight,
-            "Pressão sistólica": ap_hi,
-            "Pressão diastólica": ap_lo,
-            "Colesterol": cholesterol,
-            "Glicose": gluc,
-            "Fumante": smoke,
-            "Bebida Alcoolica": alco,
-            "Praticante de atividade física": active}
-
-    features = pd.DataFrame(data, index = [0])
+    with st.form('user_input_variebles'):
+        gender = st.sidebar.selectbox("Gênero", [1, 2], 1)
+        age = st.sidebar.slider("Idade", 15, 65, 20)
+        height = st.sidebar.slider("Altura", 0.5, 2.5, 1.70)
+        weight = st.sidebar.slider("Peso", 10, 200, 60)
+        ap_hi = st.sidebar.slider("Pressão sistólica", 0, 500, 100)
+        ap_lo = st.sidebar.slider("Pressão diastólica", 0, 500, 100)
+        cholesterol = st.sidebar.selectbox("Colesterol", [1,2 ,3], 1)
+        gluc = st.sidebar.selectbox("Glicose",[1,2, 3] ,1)
+        smoke = st.sidebar.selectbox("Fumante", [0, 1], 0)
+        alco = st.sidebar.selectbox("Bebida Alcoolica", [0 ,1], 0)
+        active = st.sidebar.selectbox("Praticante de atividade física", [0, 1], 0)
     
+        st.form_submit_button('Fazer previsão')
+
+        user_data = {"Gênero": gender,
+                "Idade": age,
+                "Altura": height,
+               "Peso": weight,
+                "Pressão sistólica": ap_hi,
+                "Pressão diastólica": ap_lo,
+                "Colesterol": cholesterol,
+                "Glicose": gluc,
+                "Fumante": smoke,
+                "Bebida Alcoolica": alco,
+                "Praticante de atividade física": active}
+
+    features = pd.DataFrame(user_data, index = [0])
+
     return features
 
-user_input_variables = user_input_features()
+user_input_varieables = user_input_features()
 
 
 # Carregando os dados
 cardio = pd.read_csv('cardio_app2.csv')
 cardio_x = cardio.drop(columns=['cardio'], axis = 1)
-df = pd.concat([user_input_variables, cardio_x], axis = 0)
+df = pd.concat([user_input_varieables, cardio_x], axis = 0)
 
 # Tabela com os dados do usuario
 st.subheader('Dados do paciente')
-st.write(user_input_variables)
+st.write(user_input_varieables)
 
 
 # Função para construção do gráfico
@@ -104,9 +108,9 @@ modelo_clf = pickle.load(open('model_randomforest.pkl', 'rb'))
 feat_importances = pd.Series(modelo_clf.feature_importances_, index=cardio_x.columns).sort_values(ascending=True)
 st.write(impPlot(feat_importances, 'Random Forest Classifier'))
 
-# Apply model to make predictions
-prediction = modelo_clf.predict(user_input_variables)
-prediction_proba = modelo_clf.predict_proba(user_input_variables)
+# Previsão
+prediction = modelo_clf.predict(user_input_varieables)
+prediction_proba = modelo_clf.predict_proba(user_input_varieables)
 
 st.subheader("Previsão: ")
 st.write("""
