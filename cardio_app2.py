c38cdab32edffada88d7e6dbd93a0a4db8d99b77
Saul Meireles <saulm@live.com>
2022-03-15 01:32:43 -0300

    initial commit

--- cardio_app2.py
+++ cardio_app2.py
@@ -8,7 +8,6 @@ import os
 from joblib import dump, load
 import sklearn
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 import pickle
 
@@ -18,15 +17,14 @@ st.write("""
 
  Aplicativo que utiliza Machine Learning para prever possíveis ocorrências de doenças do coração. \n
 
- Fonte dos dados: www.kaglle.com \n
-
+ 
  Legenda dos atributos:\n
 
  Gênero: 1 - Feminino  2 - Masculino /
  Colesterol: 1 - Normal  2 - Acima do normal  3 - Bem acima do normal /
  Glicose: 1 - Normal  2 - Acima do normal  3 - Bem acima do normal /
  Fumante: 0 - Não  1 - Sim /
- Bebiba Alcoólica: 0 - Não  1 - Sim /
+ Consumo de bebiba Alcoólica: 0 - Não  1 - Sim /
  Praticante de atividade física: 0 - Não  1 - Sim 
 """)
 
@@ -44,7 +42,7 @@ st.image('cardio_img.png')
 def user_input_features():
     with st.form('user_input_variebles'):
         gender = st.sidebar.selectbox("Gênero", [1, 2], 1)
-        age = st.sidebar.slider("Idade", 15, 65, 20)
+        age = st.sidebar.slider("Idade", 15, 80, 20)
         height = st.sidebar.slider("Altura", 0.5, 2.5, 1.70)
         weight = st.sidebar.slider("Peso", 10, 200, 60)
         ap_hi = st.sidebar.slider("Pressão sistólica", 0, 500, 100)
@@ -87,31 +85,32 @@ st.write(user_input_varieables)
 
 
 # Função para construção do gráfico
-#def impPlot(imp, name):
-#    figure = px.bar(imp,
-#                    x=imp.values,
-#                    y=imp.keys(), labels = {'x':'Importance Value', 'index':'Columns'},
-#                    text=np.round(imp.values, 2),
-#                    title=name + ' Feature Selection Plot',
-#                    width=900, height=600)
-#    figure.update_layout({
-#        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-#        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-#    })
-#    st.plotly_chart(figure)
+def impPlot(imp, name):
+    figure = px.bar(imp,
+                    x=imp.values,
+                    y=imp.keys(), labels = {'x':'Importance Value', 'index':'Columns'},
+                    text=np.round(imp.values, 2),
+                    title=name + ' Feature Selection Plot',
+                    width=900, height=600)
+    figure.update_layout({
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+    })
+    st.plotly_chart(figure)
 
 
 # Carregando o modelo
-modeloKNN = pickle.load(open('modelKNN.pkl', 'rb'))
+clf = pickle.load(open('clf_reduce.pkl', 'rb'))
 
 
 # Gráfico feat_importances
-#feat_importances = pd.Series(modeloKNN.feature_importances_, index=cardio_x.columns).sort_values(ascending=True)
-#st.write(impPlot(feat_importances, 'KNN'))
+feat_importances = pd.Series(clf.feature_importances_, index=cardio_x.columns).sort_values(ascending=True)
+st.write(impPlot(feat_importances, 'Random Forest Classifier'))
 
 # Previsão
-prediction = modeloKNN.predict(user_input_varieables)
-prediction_proba = modeloKNN.predict_proba(user_input_varieables)
+prediction = clf.predict(user_input_varieables)
+prediction_proba = clf.predict_proba(user_input_varieables)
+
 
 st.subheader("Previsão: ")
 st.write("""
